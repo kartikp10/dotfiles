d0d5469c7d0da6bf0bfdffe62c31d3716e7f46db
Kartikeya Pande <kpande479@gmail.com>
2022-07-30 10:19:55 -0400

    update installation scripts

--- install.sh
+++ install.sh
@@ -4,8 +4,13 @@ function create_links() {
     echo "creating symlinks to config files"
     local username
     username="$(whoami)"
+    rm -rf /Users/"$username"/.config/tmux
+    rm -rf /Users/"$username"/.config/nvim
+    rm -rf /Users/"$username"/.config/alacritty
+ 
     ln -s /Users/"$username"/dev/dotfiles/config/tmux /Users/"$username"/.config/tmux
     ln -s /Users/"$username"/dev/dotfiles/config/nvim /Users/"$username"/.config/nvim
+    ln -s /Users/"$username"/dev/dotfiles/config/alacritty /Users/"$username"/.config/alacritty
     echo ""
     echo "done"
 }
